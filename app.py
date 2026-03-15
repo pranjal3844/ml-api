2f0790b81fffcb001282f1db35b6d51cf6af1f75
Pranjal Kumar <pranjalkumar3844@gmail.com>
2026-03-15 18:48:46 +0530

    added web interface for prediction

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 
-# load model
 model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
 def home():
-    return "ML API is running"
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    
-    data = request.json["features"]
-    
-    prediction = model.predict([data])
 
-    return jsonify({
-        "prediction": prediction.tolist()
-    })
+    features = []
+
+    for i in range(14):
+        value = request.form.get(f"f{i}")
+        features.append(float(value))
+
+    prediction = model.predict([features])[0]
+
+    return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
