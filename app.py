cfdcb2b31982c5d5ac70722ce07a6aa0d0438556
Pranjal Kumar <pranjalkumar3844@gmail.com>
2026-03-15 18:23:59 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# load model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return "ML API is running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    
+    data = request.json["features"]
+    
+    prediction = model.predict([data])
+
+    return jsonify({
+        "prediction": prediction.tolist()
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
