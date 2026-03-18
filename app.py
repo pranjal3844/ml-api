3bd9091676c5fb1e74c682979e838873c7ee1648
Pranjal Kumar <pranjalkumar3844@gmail.com>
2026-03-18 10:55:54 +0530

    error change

--- app.py
+++ app.py
@@ -32,5 +32,7 @@ def predict():
 
     return render_template("index.html", prediction_text=f"Estimated Price: ₹ {round(prediction, 2)}")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
